c8a00737383eb8e2279402d66806653e03b8f86c
Sevinch <sevincha2008@gmail.com>
2024-10-03 15:00:14 +0500

    JS 1 dars
    Zeromax IT-center 37group

---
+++ js/main.js
@@ -0,0 +1,43 @@
+//console.log("Men faqat konsol oynasida ko'rinaman!");
+//alert("Saytimmizga xush kelibsiz!");
+
+//document.write("Men bodyda ko'rinaman");
+//document.write(9*9-71)
+
+//JS ozgaruvchilari
+
+//let x = 5;
+//let y = 7;
+//let z = 10;
+//x = 15
+//document.write(x+y+z);
+
+//let x = 15, y = 7, z = 10;
+//document.write(x+y+z)
+
+//JS ma'lumotlar turlari
+    //number string
+//let son = 45
+//let matn = "JavaScript"
+
+//document.write(typeof matn)
+
+//JSda matematika
+
+//let x = 5;
+//x+=5
+//
+//document.write(x);
+
+let x = 5
+//x*=9 
+
+x++;
+
+//let y = 15
+//y/=3 
+
+document.write(x)
+//let qoldiq = x%5
+//let daraja =x**5
+//document.write(daraja)
